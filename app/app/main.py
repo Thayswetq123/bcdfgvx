9b01ab088405040303f5dadbe828f9b7d3a47afc
Thayswetq123 <niederth@katharineum.de>
2026-02-28 02:58:27 +0100

    Create main.py

---
+++ app/app/main.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from PIL import Image
+from app.ai_engine import AIEngine
+
+st.set_page_config(layout="wide")
+
+st.title("🏢 Fundbüro AI System")
+
+engine = AIEngine()
+
+if "user" not in st.session_state:
+    st.session_state.user = None
+
+if st.session_state.user is None:
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+
+        if username == "admin" and password == "admin123":
+
+            st.session_state.user = username
+            st.rerun()
+
+        else:
+            st.error("Login Fehler")
+
+    st.stop()
+
+uploaded = st.file_uploader(
+    "Bild hochladen",
+    type=["jpg","jpeg","png"]
+)
+
+if uploaded:
+
+    image = Image.open(uploaded).convert("RGB")
+
+    result = engine.predict(image)
+
+    st.image(image, width=400)
+
+    st.success(result["label"])
+
+    st.info(
+        f"Confidence: {round(result['confidence']*100,2)}%"
+    )
